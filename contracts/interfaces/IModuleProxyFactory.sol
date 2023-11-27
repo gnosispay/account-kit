74bcf62e641c71786505005ac6ebaa7ce0d3fedf
cristovaoth <cristovao.honorato@gmail.com>
2023-11-27 20:13:21 +0100

    Regenerate interfaces

--- contracts/interfaces/IModuleProxyFactory.sol
+++ contracts/interfaces/IModuleProxyFactory.sol
@@ -3,18 +3,15 @@
 pragma solidity ^0.8.4;
 
 interface IModuleProxyFactory {
-    error FailedInitialization();
-    error TakenAddress(address address_);
-    error TargetHasNoCode(address target);
-    error ZeroAddress(address target);
-    event ModuleProxyCreation(
-        address indexed proxy,
-        address indexed masterCopy
-    );
 
-    function deployModule(
-        address masterCopy,
-        bytes memory initializer,
-        uint256 saltNonce
-    ) external returns (address proxy);
+
+error FailedInitialization(  );
+error TakenAddress( address address_ );
+error TargetHasNoCode( address target );
+error ZeroAddress( address target );
+event ModuleProxyCreation( address indexed proxy,address indexed masterCopy ) ;
+function deployModule( address masterCopy,bytes memory initializer,uint256 saltNonce ) external  returns (address proxy) ;
 }
+
+
+
