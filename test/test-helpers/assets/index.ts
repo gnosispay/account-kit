583ad70cb602fd9e7b39e5242fc54d6392606ed0
cristovaoth <cristovao.honorato@gmail.com>
2023-12-08 15:25:46 +0100

    Rename some tests helpers

--- test/test-helpers/assets/index.ts
+++ test/test-helpers/assets/index.ts
@@ -1,14 +1,14 @@
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 
-import deployDelay from "./delay";
+import deployDelayModMastercopy from "./delayModMastercopy";
 import deployFallbackHandler from "./fallbackHandler";
 import deployModuleProxyFactory from "./moduleProxyFactory";
 import deployMulticall from "./multicall";
 import deployMultisend from "./multisend";
-import deployRoles from "./roles";
+import deployRolesModMastercopy from "./rolesModMastercopy";
 import deploySafeMastercopy from "./safeMastercopy";
 import deploySafeProxyFactory from "./safeProxyFactory";
-import deploySpender from "./spender";
+import deploySpenderModMastercopy from "./spenderModMastercopy";
 import { deployFactory as deploy2470Factory } from "../factories/eip2470";
 import { deployFactory as deployNickFactory } from "../factories/nickSingletonFactory";
 import { deployFactory as deploySafeFactory } from "../factories/safeSingletonFactory";
@@ -18,13 +18,13 @@ export default async function (signer: SignerWithAddress) {
   await deployNickFactory(signer);
   await deploySafeFactory(signer);
 
-  await deployDelay(signer);
+  await deployDelayModMastercopy(signer);
   await deployFallbackHandler(signer);
   await deployModuleProxyFactory(signer);
   await deployMulticall(signer);
   await deployMultisend(signer);
-  await deployRoles(signer);
-  await deploySpender(signer);
+  await deployRolesModMastercopy(signer);
+  await deploySpenderModMastercopy(signer);
   await deploySafeMastercopy(signer);
   await deploySafeProxyFactory(signer);
 }
